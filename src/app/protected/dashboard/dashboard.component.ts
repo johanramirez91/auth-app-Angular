a75b7c4ba0cdf66c7ac60e05e4c354fe879969ff
johan.ramirez <johan.ramirez@sofka.com.co>
2022-08-27 18:36:29 -0500

    [Added] se agrega linter y se muestran datos de usuario en pantalla

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   logout() {
     this.router.navigateByUrl('/login');
+  }
+
+  get usuario() {
+    return this.authService.usuario;
   }
 }
